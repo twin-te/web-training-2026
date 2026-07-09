9351df1c6dbbbaf00404b41b09ae20e2f3a67b2b
Arata <49086220+arata-nvm@users.noreply.github.com>
2026-07-10 04:09:34 +0900

    feat: 移植前のファイルを削除

--- day1/phase2/README.md
+++
@@ -1,7 +0,0 @@
-# phase 2：CSS
-
-本フェーズでは，CSS（Cascading Style Sheets）を用いた Web サイトのスタイリングについて学習します．
-本フェーズでは，資料を PDF で配布します．
-
-- [資料（PDF）](handouts/main.pdf)
-- [演習問題の HTML](samples/index.html)
--- day3/phase1/sample/sample-app/src/components/CreateTweet.tsx
+++
@@ -1,9 +0,0 @@
-export default function CreateTweet() {
-    return (
-      <div>
-        <textarea placeholder="いまどうしてる？"></textarea>
-        <input placeholder="なまえ"></input>
-        <button>ツイート</button>
-      </div>
-    );
-}
